fee8d6fb6773ecc61991b3679a41bbfbb12d8990
VercaAbo <VercaAbo@users.noreply.github.com>
2023-02-28 20:13:04 +0000

    deploy: f593e14535dee52b0d02746ccfc55fc5f2e0c59a

--- _app/immutable/chunks/4-65a69fc5.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/Pravidla/_page.svelte-4f46ec62.js";export{t as component};
---
+++ _app/immutable/chunks/4-9477fc48.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/Pravidla/_page.svelte-17715af3.js";export{t as component};
