e85a3e53481605efcc35f57081b1514333303ffa
VercaAbo <VercaAbo@users.noreply.github.com>
2023-03-01 20:46:24 +0000

    deploy: bf7bb115e4c97398acd87b32810ee55cffc6e5b2

---
+++ _app/immutable/chunks/6-11e56a9d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/pocitani/_page.svelte-afb4d35b.js";export{t as component};
--- _app/immutable/chunks/6-5fddbc81.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/pocitani/_page.svelte-a61220ee.js";export{t as component};
